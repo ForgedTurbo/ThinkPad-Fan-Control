2672f290ee8c115c14d2252d60cbfb8a6c51c5b3
ForgedTurbo <41134112+ForgedTurbo@users.noreply.github.com>
2019-09-24 14:03:38 -0600

    Update tpfc_start-up.sh
    Attempt Fixing the Script for Newer Linux

--- tpfc_start-up.sh
+++ tpfc_start-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sleep 15 &&
 TP_PATH=`pwd`
-TP_PATH='/home/stanko/Projects/fans/'
+TP_PATH='/home/ForgedTurbo/Projects/Fan-Control/'
 cd $TP_PATH
-gksu data/tp_fan_control &
+sudo -c data/tp_fan_control &
 exit
